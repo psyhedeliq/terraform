79135482b46b54cea38b3c9206a494ef9f6ccd2e
Cornel Filip <filipcornelcristian@gmail.com>
2024-07-16 16:54:09 +0100

    feat: Use AWS credentials variables

--- variables.tf
+++ variables.tf
@@ -3,6 +3,18 @@ variable "aws_region" {
   default = "us-east-1"
 }
 
+variable "aws_access_key_id" {
+  description = "AWS access key"
+  type        = string
+  sensitive   = true
+}
+
+variable "aws_secret_access_key" {
+  description = "AWS secret key"
+  type        = string
+  sensitive   = true
+}
+
 variable "vpc_name" {
   type    = string
   default = "demo_vpc"
