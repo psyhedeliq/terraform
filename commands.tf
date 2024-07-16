9f60d70ad939476bb80f902fb7c14ebd27069dda
Cornel Filip <filipcornelcristian@gmail.com>
2024-07-16 13:59:33 +0100

    feat: Add commands.tf file with Terraform commands

---
+++ commands.tf
@@ -0,0 +1,14 @@
+# Command to init the terraform:
+# terraform init
+
+# Command to validate the terraform:
+# terraform validate
+
+# Command to plan the changes:
+# terraform plan
+
+# Command to apply the changes:
+# terraform apply -auto-approve
+
+# Command to destroy the changes:
+# terraform destroy -auto-approve
