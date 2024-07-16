3a053de1e395000c2800ed6bfe4a893c3a7b8e8b
Cornel Filip <filipcornelcristian@gmail.com>
2024-07-16 13:23:52 +0100

    fix: invalid index error

--- variables.tf
+++ variables.tf
@@ -17,7 +17,6 @@ variable "private_subnets" {
   default = {
     "private_subnet_1" = 1
     "private_subnet_2" = 2
-    "private_subnet_3" = 3
   }
 }
 
@@ -25,6 +24,5 @@ variable "public_subnets" {
   default = {
     "public_subnet_1" = 1
     "public_subnet_2" = 2
-    "public_subnet_3" = 3
   }
 }
