64fc017b4e5f99c69905a3478656b1551772f8d1
Cornel Filip <filipcornelcristian@gmail.com>
2024-07-16 14:40:41 +0100

    feat: Add commands for managing Terraform state

--- commands.tf
+++ commands.tf
@@ -1,5 +1,14 @@
+# List of commands to manage terraform:
+# ======================================
+
 # Command to init the terraform:
 # terraform init
+
+# Command to show the state of our resources that were deployed to AWS:
+# terraform show
+
+# Command to list the resources in the state file (a list of resources that is currenty managed by terraform):
+# terraform state list
 
 # Command to validate the terraform:
 # terraform validate
