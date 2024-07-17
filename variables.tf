cca4367c83aa608c06722fb4a8490f1a26ab515c
Cornel Filip <filipcornelcristian@gmail.com>
2024-07-17 13:37:02 +0100

    feat: Add variables for Variables Subnet

--- variables.tf
+++ variables.tf
@@ -44,3 +44,21 @@ variable "public_subnets" {
     "public_subnet_2" = 2
   }
 }
+
+variable "variables_sub_cidr" {
+  description = "CIDR Block for the Variables Subnet"
+  type        = string
+  default     = "10.0.250.0/24"
+}
+
+variable "variables_sub_az" {
+  description = "Availability Zone used Variables Subnet"
+  type        = string
+  default     = "eu-west-2"
+}
+
+variable "variables_sub_auto_ip" {
+  description = "Set Automatic IP Assigment for Variables Subnet"
+  type        = bool
+  default     = true
+}
