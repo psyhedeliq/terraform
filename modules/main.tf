03596751552636b1ddf64490d460db3d9f4f757a
Cornel Filip <filipcornelcristian@gmail.com>
2024-07-29 15:53:51 +0300

    feat: Add module for subnet addresses with variable configuration

---
+++ modules/main.tf
@@ -0,0 +1,21 @@
+module "subnet_addrs" {
+  source  = "hashicorp/subnets/cidr"
+  version = "1.0.0"
+
+  base_cidr_block = "10.0.0.0/22"
+  networks = [
+    {
+      name     = "module_network_a"
+      new_bits = 2
+    },
+    {
+      name     = "module_network_b"
+      new_bits = 2
+    },
+  ]
+}
+
+// Export information about the module into the cli
+output "subnet_addrs" {
+  value = module.subnet_addrs.network_cidr_blocks
+}
